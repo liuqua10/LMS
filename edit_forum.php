7e3b0fc4f629e751d9be860c8c481728bc94ee01
Quanhui Liu <liuqua10@163.com>
2014-12-22 18:48:06 -0800

    Create edit_forum.php

---
+++ edit_forum.php
@@ -0,0 +1,31 @@
+<?php 
+	require('../connect.php');
+	if (isset($_GET['sub_id'])) {
+		$id = $_GET['sub_id'];
+		$query = "SELECT * FROM subtask WHERE sub_id = '$id';";
+		$prep = $db->prepare($query);
+		$prep->execute();
+		$results = $prep->fetchAll(PDO::FETCH_ASSOC);
+		foreach($results as $result) {
+			$name = $result['name'];
+			$content = $result['content'];
+		}
+	} elseif (isset ($_POST['sub_id'])) {
+		$id = $_POST['sub_id'];
+		$name = $_POST['title'];
+		$content = $_POST['content'];
+		$query = "UPDATE subtask SET name ='$name', content = '$content' WHERE sub_id = '$id';";
+		$prep = $db->prepare($query);
+		$prep->execute();
+		echo "Updated!!";
+	} else {
+		echo "NO POST SELECTED";
+	}
+?> 
+
+<form action = "edit_forum.php" method = "POST"> 
+	<p>Title: <input type = "text" value = "<?= $name ?>" name= "title"></p>
+	<p>Message: <textarea name = "content"><?= $content?></textarea></p>
+	<input type ="hidden" name = "sub_id" value = "<?= $id ?>">
+	<input type="submit" value = "Submit">
+</from> 
