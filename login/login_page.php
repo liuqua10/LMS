5578d31296aefb433c9537042effbc26be159897
Quanhui Liu <liuqua10@163.com>
2015-07-08 17:42:32 -0700

    Login page and redirect

--- login/login_page.php
+++ login/login_page.php
@@ -1,18 +1,62 @@
-<?php 
-	include ("common.php");
-	top();
-?> 
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Please Signup </title>
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+    <!-- Angular js -->
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+    <!-- Validation js -->
+    <script src="app.js"></script>
+</head>
+
+<body class="register index">
+
+    <div class="container">
+
+        <!-- PAGE HEADER -->
+        <div class="page-header row">
+            <h1>Please Login</h1>
+
+        </div>
 
 
-				<form class = "loginform" id="loginform" action="login.php" method="post">
-					<h1>Welcome Earthlings</h1>
-					<div class="signup">Don't have an account?&thinsp;<a href="signup_page.php">Sign up!</a></div>
-					<div class = "enter username"><input name="username/Email" type="text" size="30" autofocus="autofocus" placeholder="Username/Email" /></div>
-					<div class = "enter password"><input name="password" type="password" size="30" placeholder= "Password" /></div>
-					<div class = "forget">Forget <a href="retrieve_page.php">username/password?</a></div>
-					<div class = "stay"><input type = "checkbox"/>Stay sign in</div>
-					<div class = "loginbtn"><input class = "btn" type="submit" value="Log in" /></div>
-				</form>
-			</div>
-	</body>
+        <!-- ============FORM================ -->
+
+
+      <!-- pass in the variable if form is valid or invalid -->
+        <form ng-app="myApp" ng-controller="validateCtrl" name="myForm" novalidate>
+
+
+            <!-- USERNAME -->
+            <div class="form-group" required>
+                <label>Username</label>
+                <input type="text" name="user" ng-model="user" class="form-control" required>
+                <span style="color:red" ng-show="myForm.user.$dirty && myForm.user.$invalid">
+                <span ng-show="myForm.user.$error.required">Username is required.</span>
+                </span>
+            </div>
+
+
+            <!-- PASSWORD -->
+            <div class="form-group" required>
+                <label>Password (between 3 to 8 charaters)</label>
+                <input type="password" name="password" class="form-control" ng-model="password" ng-minlength="3" ng-maxlength="8">
+                <span style="color:red" ng-show="myForm.password.$dirty && myForm.password.$invalid">
+                <span ng-show="myForm.password.$error.required">Password is required.</span>
+                <span ng-show="myForm.password.$error.minlength">Password is too short.</span>
+                <span ng-show="myForm.password.$error.maxlength">Password is too long.</span>
+                </span>
+            </div>
+
+
+
+              <!-- Submit -->
+            <button type="submit" class="btn btn-primary">Submit</button>
+						<button class="btn" onclick="location.href='signup_page.php'">Sign up</button>
+
+        </form>
+</body>
+
 </html>
